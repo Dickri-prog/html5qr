1b356be3235ad6de10c26d7432df9ef31141db46
unknown <thedickraygomoiy58@gmail.com>
2023-04-11 11:42:41 +0700

    update ui

--- WriteToTxt.php
+++ WriteToTxt.php
@@ -7,12 +7,10 @@ if (isset($_GET['id'])) {
   if ($txt != "") {
     $myfile = file_put_contents('id_order.txt', $txt.PHP_EOL , FILE_APPEND | LOCK_EX);
     echo json_encode([
-      "code" => 200,
       "message" => "success"
     ]);
   }else {
     echo json_encode([
-      "code" => 400,
       "message" => "Empty Value!!!"
     ]);
   }
